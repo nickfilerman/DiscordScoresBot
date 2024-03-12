9d52690dd6734fd2fd808bf19b7c4d18e8300692
nickfilerman <filerman@msu.edu>
2024-03-12 03:46:53 +0000

    got hybrid commands working

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+
 from helpers import getScores, getSchools, prettier
 from discord.ext import commands
 
@@ -6,7 +7,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all(), help_command=None)
 
 '''
-Sets up bot and messages bot channel that it is online
+Sets up bot, syncs slash (hybrid) commands, and messages bot channel that it is online
 '''
 @bot.event
 async def on_ready():
@@ -14,101 +15,92 @@ async def on_ready():
   bot.schoolDict = getSchools()
 
   try:
-    synced = await bot.tree.sync(guild=discord.Object(id=bot.serverId))
+    synced = await bot.tree.sync()
     await bot.channel.send(f'{bot.user.name} is online! Synced {len(synced)} commands')
 
-  except:
+  except Exception as e:
    await bot.channel.send('Error syncing')
 
 '''
 Sends error message to bot chonnel, on error
 
-:param message: Message sent that contained the error
+:param ctx: The context surrounding the message sent for the command that contained an error
 :param error: The error itself
 '''
 @bot.event
-async def on_command_error(message, error):
-  if message.message.content[1] == '!':
+async def on_command_error(ctx, error):
+  if ctx.message.content[1] == '!':
     return
   
   if isinstance(error, commands.CommandNotFound):
-    await message.send(str(error))
+    await ctx.send(str(error))
   else:
-    await message.send('An Error Occurred. It is Likely that No Scores are Available at This Time')
+    await ctx.send('An Error Occurred. It is Likely that No Scores are Available at This Time')
 
-  await bot.channel.send(str(error) + '\n\nCommand: ' + message.message.content)
+  await bot.channel.send(str(error) + '\n\nCommand: ' + ctx.message.content)
   
 '''
 Gets the score for the default gid and given sport (or all sports) and returns as message
 
-:param message: The message and relevant information provided with the message
+:param ctx: The context surrounding the message sent for the command
 :param sport: What sport to filter the scores for, default is "all" if you want all sports
 '''
-@bot.tree.command(name='score') 
-async def score(message, sport: str):
-  if not sport or sport == '':
-    sport = 'all'
-
+@bot.hybrid_command(name='score') 
+async def score(ctx, sport: str='all'):
   returnStr = prettier(getScores(sport=sport, gid=bot.defaultGid))
   if returnStr == '':
     returnStr = 'No Scores Available'
   
-  await message.send(returnStr)
+  await ctx.send(returnStr)
 
 '''
 Gets the score for the default gid and given sport (or all sports) that are currently ongoing and returns as message
 
-:param message: The message and relevant information provided with the message
+:param ctx: The context surrounding the message sent for the command
 :param sport: What sport to filter the scores for, default is "all" if you want all sports
 '''
-@bot.tree.command(name='now_score')
-async def now_score(message, sport: str):
-  if not sport or sport == '':
-    sport = 'all'
-
+@bot.hybrid_command(name='now_score')
+async def now_score(ctx, sport:  str='all'):
   returnStr = prettier(getScores(sport=sport, gid=bot.defaultGid, now=True))
   if returnStr == '':
     returnStr = 'No Scores Available'
 
-  await message.send(returnStr)
+  await ctx.send(returnStr)
 
 '''
 Gets the score for another team (defaults to default gid) and given sport (or all sports) and returns as message
 
-:param message: The message and relevant information provided with the message
+:param ctx: The context surrounding the message sent for the command
 :param gid: What gid to get the scores from, gets set to bot.defaultGid if nothing is passed
 :param sport: What sport to filter the scores for, default is "all" if you want all sports
 '''
-@bot.tree.command(name='other_score')
-async def other_score(message, gid: str, sport: str):
-  if not gid or gid == '':
+@bot.hybrid_command(name='other_score')
+async def other_score(ctx, gid: str='', sport: str='all'):
+  if gid == '':
     gid = bot.defaultGid
-
-  if not sport or sport == '':
-    sport = 'all'
 
   if gid.lower() in bot.schoolDict:
     returnStr = prettier(getScores(sport=sport, gid=gid))
     if returnStr == '':
       returnStr = 'No Scores Available'
 
-    await message.send(returnStr)
+    await ctx.send(returnStr)
   else:
-    await message.send('Not a Valid School GID, Check statbroadcast.com/events/all.php')
+    await ctx.send('Not a Valid School GID, Check statbroadcast.com/events/all.php')
 
 '''
 Returns as message, descriptions for each command available with formatting
 
-:param message: The message and relevant information provided with the message
+:param ctx: The context surrounding the message sent for the command
 '''
-@bot.tree.command(name='help')
-async def help(message):
+@bot.hybrid_command(name='help')
+async def help(ctx):
   helpDict = {'score': 'Gets all recent and ongoing scores for ' + bot.defaultGid + ' sports, add sport name to get only scores for that sport (example: !score basketball)\n\n',
-              'nowScore': 'Gets all ongoing scores for ' + bot.defaultGid + ' sports, add sport name to get only scores for that sport (example: !nowScore hockey)\n\n',
-              'otherScore': 'Gets all ongoing scores for a given school\'s Sports, add sport name to get only scores for that sport (example: !otherScore ' + bot.defaultGid + ' football)\n    school name must be equal to the \'gid\' given by the school\'s relevant statbroadcast page, the list of schools can be found at statbroadcast.com/events/all.php'}
+              'now_score': 'Gets all ongoing scores for ' + bot.defaultGid + ' sports, add sport name to get only scores for that sport (example: !nowScore hockey)\n\n',
+              'other_score': 'Gets all ongoing scores for a given school\'s Sports, add sport name to get only scores for that sport (example: !otherScore ' + bot.defaultGid + ' football)\n    school name must be equal to the \'gid\' given by the school\'s relevant statbroadcast page, the list of schools can be found at statbroadcast.com/events/all.php'}
   
   returnStr = '```help:\n'
   for helpCommand, helpDesc in helpDict.items():
     returnStr += '  ' + helpCommand + ': ' + helpDesc
 
-  await message.send(returnStr + '\n```')
+  await ctx.send(returnStr + '\n```')
