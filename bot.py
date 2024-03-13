5068287e46a34811e70426201fe61ab6f929df8f
nickfilerman <filerman@msu.edu>
2024-03-13 16:04:28 -0400

    added more typing

--- bot.py
+++ bot.py
@@ -23,16 +23,16 @@ Sends error message to bot chonnel, on error
 :param error: The error itself
 '''
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx: commands.Context, error: commands.CommandError):
   if ctx.message.content[1] == '!':
     return
+  
+  await bot.channel.send(str(error) + '\n\nCommand: ' + ctx.message.content)
   
   if isinstance(error, commands.CommandNotFound):
     await ctx.send(str(error))
   else:
     await ctx.send('An Error Occurred. It is Likely that No Scores are Available at This Time')
-
-  await bot.channel.send(str(error) + '\n\nCommand: ' + ctx.message.content)
 
 '''
 Syncs hybrid_commands with Discord to use for slash commands (validates user first)
@@ -44,10 +44,9 @@ async def sync(ctx: commands.Context):
   if (ctx.permissions.administrator):
     try:
       synced = await bot.tree.sync()
-      await bot.channel.send(f'Synced {len(synced)} commands')
-
-    except Exception as e:
-     await bot.channel.send('Error syncing')
+      await bot.channel.send(f'Synced {len(synced)} commands') 
+    except:
+      await bot.channel.send('Error syncing')
   else:
     await ctx.send('Command "sync" is not found')
   
@@ -58,7 +57,7 @@ Gets the score for the default gid and given sport (or all sports) and returns a
 :param sport: What sport to filter the scores for, default is "all" if you want all sports
 '''
 @bot.hybrid_command(name='score') 
-async def score(ctx, sport: str='all'):
+async def score(ctx: commands.Context, sport: str='all'):
   returnStr = prettier(getScores(sport=sport, gid=bot.defaultGid))
   if returnStr == '':
     returnStr = 'No Scores Available'
@@ -72,7 +71,7 @@ Gets the score for the default gid and given sport (or all sports) that are curr
 :param sport: What sport to filter the scores for, default is "all" if you want all sports
 '''
 @bot.hybrid_command(name='now_score')
-async def now_score(ctx, sport:  str='all'):
+async def now_score(ctx: commands.Context, sport: str='all'):
   returnStr = prettier(getScores(sport=sport, gid=bot.defaultGid, now=True))
   if returnStr == '':
     returnStr = 'No Scores Available'
@@ -87,7 +86,7 @@ Gets the score for another team (defaults to default gid) and given sport (or al
 :param sport: What sport to filter the scores for, default is "all" if you want all sports
 '''
 @bot.hybrid_command(name='other_score')
-async def other_score(ctx, gid: str='', sport: str='all'):
+async def other_score(ctx: commands.Context, gid: str='', sport: str='all'):
   if gid == '':
     gid = bot.defaultGid
 
@@ -106,7 +105,7 @@ Returns as message, descriptions for each command available with formatting
 :param ctx: The context surrounding the message sent for the command
 '''
 @bot.hybrid_command(name='help')
-async def help(ctx):
+async def help(ctx: commands.Context):
   helpDict = {'score': 'Gets all recent and ongoing scores for ' + bot.defaultGid + ' sports, add sport name to get only scores for that sport (example: !score basketball)\n\n',
               'now_score': 'Gets all ongoing scores for ' + bot.defaultGid + ' sports, add sport name to get only scores for that sport (example: !nowScore hockey)\n\n',
               'other_score': 'Gets all ongoing scores for a given school\'s Sports, add sport name to get only scores for that sport (example: !otherScore ' + bot.defaultGid + ' football)\n    school name must be equal to the \'gid\' given by the school\'s relevant statbroadcast page, the list of schools can be found at statbroadcast.com/events/all.php'}
